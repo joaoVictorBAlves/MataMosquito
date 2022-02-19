343ae0bcb89626b4e632ad13dce4b7d19bbd8f98
João Victor Alves <joaovba2002@alu.ufc.br>
2022-02-19 16:09:00 -0300

    Jogo Final

--- jogo.js
+++ jogo.js
@@ -2,6 +2,21 @@
 var altura = 0
 var largura = 0
 var vidas = 1
+var tempo = 7
+var criaMosquitoTempo = 1500
+
+var nivel = window.location.search
+nivel = nivel.replace('?', '')
+
+if (nivel === 'normal') {
+    criaMosquitoTempo = 1500
+} else if (nivel === 'dificil') {
+    criaMosquitoTempo = 1000
+} else if (nivel === 'chucknorris') {
+    criaMosquitoTempo = 750
+}
+console.log("tempo: ",criaMosquitoTempo)
+
 const ajustaPalco = () => {
     altura = window.innerHeight
     largura = window.innerWidth
@@ -11,14 +26,27 @@ const ajustaPalco = () => {
 
 ajustaPalco()
 
+var cronometro = setInterval(() => {
+    document.getElementById('cronometro').innerHTML = tempo
+    tempo -= 1
+
+    if (tempo < 0) {
+        clearInterval(cronometro)
+        clearInterval(criaMosca)
+        window.location.href = 'vitoria.html'
+    }
+}, 1000)
 //Gerando posições randômicas
 function posicaoAleatoria() {
     //Remover mosquito anterior
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
-        document.getElementById('v'+vidas).src = 'imagens/coracao_vazio.png'
-
-        vidas++
+        if (vidas > 3) {
+            window.location.href = 'fim_de_jogo.html'
+        } else {
+            document.getElementById('v' + vidas).src = 'imagens/coracao_vazio.png'
+            vidas++
+        }
     }
     //Definir posições
     var posicaoX = Math.floor(Math.random() * largura) - 100
@@ -37,7 +65,7 @@ function posicaoAleatoria() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.id = 'mosquito'
-    mosquito.onclick = function(){
+    mosquito.onclick = function () {
         this.remove()
     }
 
